6cb4c4a811b646b566488a93d14b1f289607a151
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-01-01 15:36:02 -0300

    refatoração scroll-suave.js

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,17 +1,33 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]'
-  );
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    //Define padrões, quando não fornecidos
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    //bind = mantém contexto do bind correto
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
 
-  function scrollToSection(e) {
+  scrollToSection(e) {
     e.preventDefault();
     const href = e.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
-
-    section.scrollIntoView({ behavior: "smooth", block: "start" });
+    section.scrollIntoView(this.options);
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
