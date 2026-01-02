9ad0cba4804c71b703d927fa63f37eb314f8eb2d
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-01-01 22:53:25 -0300

    refatoração do animaNumeros para class

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
+import initFetchAnimals from "./modules/fetch-animais.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
@@ -31,9 +31,8 @@ modal.init();
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
 
-
+initFetchAnimals();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
 initAnimacaoScroll();
