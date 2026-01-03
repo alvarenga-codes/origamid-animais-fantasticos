05ab43c31c749c414d45d4984fc9431df7e9735b
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-01-03 18:28:39 -0300

    refatoração do dowpdownmenu para class

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,38 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
-
-  dropdownMenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
-    });
-  });
-
-  function handleClick(e) {
+export default class DropdownMenu {
+  constructor(dropdownmenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownmenus);
+    this.activeClass = "active";
+    //define touchstart e click como arg padrão de events
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
+    //Ajusta o this da função
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+//Ativa o dropdownmenu e adiciona a função que
+//observa o clique fora dele
+  activeDropdownMenu(e) {
     e.preventDefault();
-    this.classList.add("active");
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("active");
+    const element = e.currentTarget;
+    this.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
+  }
+  //Adiciona os evetnso ao dropdownmenu
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
+    });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import ScrollAnima from "./modules/scroll-anima.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
+import DropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 
@@ -37,6 +37,8 @@ fetchAnimais('../../animaisapi.json', '.numeros-grid');
 const scrollAnima = new ScrollAnima("[data-anime='scroll']");
 scrollAnima.init();
 
-initDropdownMenu();
+const dropdownMenu = new DropdownMenu("[data-dropdown]");
+dropdownMenu.init();
+
 initMenuMobile();
 initFuncionamento();
