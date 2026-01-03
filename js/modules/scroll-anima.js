7c9ba6eb69416f33114e15089cd0c1e332663118
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-01-03 17:46:33 -0300

    cricação da função debounce

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,11 +1,11 @@
-import setFunctionName from "@babel/runtime/helpers/setFunctionName";
+import debounce from './debounce.js';
 
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
   //Verifica a distância em cada objeto
   //em relação ao scroll do site
