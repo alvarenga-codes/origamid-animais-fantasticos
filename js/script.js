b344605c751411658a517f96ced978ea7057f1f1
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-01-05 21:01:29 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import ScrollAnima from "./modules/scroll-anima.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
 import Funcionamento from "./modules/funcionamento.js";
+import SlideNav from "./modules/slide.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -44,3 +45,7 @@ menuMobile.init();
 
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
+
+const slide = new SlideNav(".slide", ".slide-wrapper");
+slide.init();
+slide.addControl(".custom-controls");
